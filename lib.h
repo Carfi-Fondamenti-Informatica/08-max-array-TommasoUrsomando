b4104ac6a67026b62f8fe381e3e352f0114943f0
TommasoUrsomando <115471825+TommasoUrsomando@users.noreply.github.com>
2022-12-09 17:28:44 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,7 @@
-floatmaggiore (float array[],int dimensione);
+#ifndef ESERCITAZIONE8_LIB_H
+
+#define ESERCITAZIONE8_LIB_H
+
+float maggiore(float array[], int dimensione);
+
+#endif//ESERCITAZIONE8_LIB_H
